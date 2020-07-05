684b8adc8a2cea52819ae62a534d18a0d9b6e5f1
Diana kagotho <dianakago2@gmail.com>
2020-07-05 00:19:19 -0400

    changed format

--- client/src/components/pages/viewall/ViewAll.js
+++ client/src/components/pages/viewall/ViewAll.js
@@ -6,50 +6,58 @@ import togatherList from "../supplementary/togather.json";
 // CSS
 import "../../css/mainwindow.css";
 import "../../css/card.css";
-import ReactSearchBox from 'react-search-box';
-
+import ReactSearchBox from "react-search-box";
 
 class ViewAll extends React.Component {
-    state = {
+  state = {
     togatherList,
   };
 
   data = [
     {
-      key: '1',
-      value: 'Soccer',
+      key: "1",
+      value: "Soccer",
     },
     {
-      key: '2',
-      value: 'Volley',
+      key: "2",
+      value: "Volley",
     },
     {
-      key: '3',
-      value: 'Basketball',
+      key: "3",
+      value: "Basketball",
     },
     {
-      key: '4',
-      value: 'Tennis',
+      key: "4",
+      value: "Tennis",
     },
-  ]
-  
+  ];
+
   render() {
     return (
       <div className="content-wrapper">
         <section className="content-header">
           <h1>
-          {/* {eventObject[0][0].headtitle} */}
-            <small><i>"Start something new or join in.."</i></small>
+            <small>
+              <i>"Start something new or join in.."</i>
+            </small>
           </h1>
           <ol className="breadcrumb">
             <li>
-              <a href="/"><i className="fas fa-door-open"></i><span className="text-orange"> Main</span></a>
+              <a href="/">
+                <i className="fas fa-door-open"></i>
+                <span className="text-orange"> Main</span>
+              </a>
             </li>
             <li>
-            <a href="/events"><span className="text-orange"> Events</span></a>
+              <a href="/events">
+                <span className="text-orange"> Events</span>
+              </a>
             </li>
             <li>
-              <a href="/viewall"><i className="fas fa-door-open"></i><span className="text-orange"> View All</span></a>
+              <a href="/viewall">
+                <i className="fas fa-door-open"></i>
+                <span className="text-orange"> View All</span>
+              </a>
             </li>
           </ol>
         </section>
@@ -58,61 +66,59 @@ class ViewAll extends React.Component {
           <h2>View All</h2>
           <form>
             <div className="row">
-            <select className="title-container" value={this.state.games}>
-              <option value="0">Sort</option>
-              <option value="1">Soccer</option>
-              <option value="2">Volley</option>
-              <option value="3">Basketball</option>
-              <option value="4">Tennis</option>
-            </select>
-            <div className="title-container"></div>
-            <div className="title-container">
-            <ReactSearchBox
-        placeholder="search"
-        value=""
-        data={this.data}
-        callback={record => console.log(record)}
-      /></div>
-       </div> 
-  
-      <div className="row">
-      <div className="title-container">
-            <ReactSearchBox
-        placeholder="Title"
-        value=""
-        data={this.data}
-        callback={record => console.log(record)}
-      /></div>
+              <select className="title-container" value={this.state.games}>
+                <option value="0">Sort</option>
+                <option value="1">Soccer</option>
+                <option value="2">Volley</option>
+                <option value="3">Basketball</option>
+                <option value="4">Tennis</option>
+              </select>
+              <div className="title-container"></div>
+              <div className="title-container">
+                <ReactSearchBox
+                  placeholder="search"
+                  value=""
+                  data={this.data}
+                  callback={(record) => console.log(record)}
+                />
+              </div>
+            </div>
 
-      <div className="title-container">
-            <ReactSearchBox
-        placeholder="Location"
-        value=""
-        data={this.data}
-        callback={record => console.log(record)}
-      /></div>
+            <div className="row">
+              <div className="title-container">
+                <ReactSearchBox
+                  placeholder="Title"
+                  value=""
+                  data={this.data}
+                  callback={(record) => console.log(record)}
+                />
+              </div>
 
-      <div className="title-container">
-            <ReactSearchBox
-        placeholder="Time"
-        value=""
-        data={this.data}
-        callback={record => console.log(record)}
-      /></div>
-      </div>
+              <div className="title-container">
+                <ReactSearchBox
+                  placeholder="Location"
+                  value=""
+                  data={this.data}
+                  callback={(record) => console.log(record)}
+                />
+              </div>
 
-    <button class="btn"> Submit</button>
+              <div className="title-container">
+                <ReactSearchBox
+                  placeholder="Time"
+                  value=""
+                  data={this.data}
+                  callback={(record) => console.log(record)}
+                />
+              </div>
+            </div>
+
+            <button class="btn"> Submit</button>
           </form>
         </section>
-
       </div>
-      
     );
   }
-  
-
 }
-
-
 
 export default ViewAll;
