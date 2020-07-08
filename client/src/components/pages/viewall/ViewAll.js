448278a2a2efd45907c067f1e3a2bc414084958e
Diana kagotho <dianakago2@gmail.com>
2020-07-07 21:22:48 -0400

    incorporated the togather.json into viewall.js

--- client/src/components/pages/viewall/ViewAll.js
+++ client/src/components/pages/viewall/ViewAll.js
@@ -2,36 +2,24 @@ import React from "react";
 // import React, { Component } from "react";
 import "../../css/viewall.css";
 import CardEvent from "../supplementary/CardEvent";
-import togatherList from "../supplementary/togather.json";
+
 // CSS
 import "../../css/mainwindow.css";
 import "../../css/card.css";
 import ReactSearchBox from "react-search-box";
 
+const events = [];
+ fetch("../supplementary/togather.json")
+.then(response => response.json())
+.then(data => events = data);
+
 class ViewAll extends React.Component {
+  togatherList = require("../supplementary/togather.json");
+
   state = {
-    togatherList,
+    savedEvents : events ?? [],
   };
-
-  data = [
-    {
-      key: "1",
-      value: "Soccer",
-    },
-    {
-      key: "2",
-      value: "Volley",
-    },
-    {
-      key: "3",
-      value: "Basketball",
-    },
-    {
-      key: "4",
-      value: "Tennis",
-    },
-  ];
-
+  
   render() {
     return (
       <div className="content-wrapper">
@@ -73,87 +61,45 @@ class ViewAll extends React.Component {
                 />
               </div>
             </div>
-            <div class="homeT_wrapper">
-        <div class="homeT_table">
-          <div class="homeT_row homeT_header homeT_green">
-            <div class="homeT_cell">
-              Title
+            <div className="homeT_wrapper">
+              <div className="homeT_table">
+                <div className="homeT_row homeT_header homeT_green">
+                  <div className="homeT_cell">Title</div>
+                  <div className="homeT_cell">Location</div>
+                  <div className="homeT_cell">Event Date</div>
+                  <div className="homeT_cell">Participants</div>
+                  <div className="homeT_cell">Start Time</div>
+                  <div className="homeT_cell">End Time</div>
+                  <div className="homeT_cell">Status</div>
+                </div>
+
+                {this.state.savedEvents.map((event) => (
+                  <div className="homeT_row">
+                    <div className="homeT_cell" data-title="Event Name">
+                      {event.eventTitle}
+                    </div>
+                    <div className="homeT_cell" data-title="Location">
+                      {event.eventLocation}
+                    </div>
+                    <div className="homeT_cell" data-title="Event Date">
+                      {event.eventDate}
+                    </div>
+                    <div className="homeT_cell" data-title="Participants">
+                      {event.eventParticipants}
+                    </div>
+                    <div className="homeT_cell" data-title="Start Time">
+                      {event.eventStartTime}
+                    </div>
+                    <div className="homeT_cell" data-title="End Time">
+                      {event.eventEndTime}
+                    </div>
+                    <div className="homeT_cell" data-title="Status">
+                      {event.eventStatus}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-            <div class="homeT_cell">
-              Location
-            </div>
-            <div class="homeT_cell">
-              Event Date
-            </div>
-            <div class="homeT_cell">
-              Time
-            </div>
-          </div>    
-          <div class="homeT_row">
-            <div class="homeT_cell" data-title="Event Name">
-              Event 1
-            </div>
-            <div class="homeT_cell" data-title="Participants">
-              10
-            </div>
-            <div class="homeT_cell" data-title="Event Date">
-              09/15/2020
-            </div>
-            <div class="homeT_cell" data-title="Location">
-              8:00 AM
-            </div>
-            
-          </div>
-          
-          <div class="homeT_row">
-            <div class="homeT_cell" data-title="Event Name">
-              Event 2
-            </div>
-            <div class="homeT_cell" data-title="Participants">
-              40
-            </div>
-            <div class="homeT_cell" data-title="Event Date">
-              10/1/2020
-            </div>
-            <div class="homeT_cell" data-title="Location">
-              10:30 AM
-            </div>
-            
-          </div>
-          
-          <div class="homeT_row">
-            <div class="homeT_cell" data-title="Event Name">
-              Event 3
-            </div>
-            <div class="homeT_cell" data-title="Participants">
-              5
-            </div>
-            <div class="homeT_cell" data-title="Event Date">
-              10/20/2020
-            </div>
-            <div class="homeT_cell" data-title="Location">
-              1:30 PM
-            </div>
-            
-          </div>
-          
-          <div class="homeT_row">
-            <div class="homeT_cell" data-title="Event Name">
-              Event 4
-            </div>
-            <div class="homeT_cell" data-title="Participants">
-              20
-            </div>
-            <div class="homeT_cell" data-title="Event Date">
-              11/20/2020
-            </div>
-            <div class="homeT_cell" data-title="Location">
-              5:00 PM
-            </div>
-            
-          </div>
-        </div>
-      </div>
           </form>
         </section>
       </div>
